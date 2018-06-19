243e80d0fe2098b21f418dad1fc75d83231a1415
Bucky Townsend <adam@desnudopenguino.com>
2018-06-19 01:23:13 -0700

    added some simple validations

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,3 +1,9 @@
 class Receipt < ApplicationRecord
     has_many :items
+
+    validates :total, presence: true
+    validates :tax, presence: true
+    validates :store, presence: true
+    validates :purchase_date, presence: true
+
 end
