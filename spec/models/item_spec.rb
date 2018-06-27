c26e00b56e1ad9abb0630d32ae2f3bc9b3379c84
Bucky Townsend <adam@desnudopenguino.com>
2018-06-26 17:22:52 -0700

    added item and receipt specs, and started writing tests for items

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe Item, type: :model do
+	it { is_expected.to validate_presence_of(:name) }
+	it { is_expected.to validate_presence_of(:type) }
+	it { is_expected.to validate_presence_of(:cost) }
+	it { is_expected.to validate_presence_of(:qty) }
+	it { is_expected.to validate_presence_of(:receipt_id) }
+end
