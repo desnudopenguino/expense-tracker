24119b052717872698234fd72a3c6c20656dba3e
Bucky Townsend <adam@desnudopenguino.com>
2018-07-04 10:37:41 -0700

    started writing specs for controllers

---
+++ spec/controllers/receipt_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe ReceiptsController do
+	it "renders the index view" do
+		rec = create(:receipt)
+		get :index
+		expect(response).to be_success
+		expect(response).to render_template("index")
+	end
+	it "renders the show view" do
+		rec = create(:receipt)
+		get :show, params: { id: rec.id }
+		expect(response).to be_success
+		expect(response).to render_template("show")
+	end
+end
