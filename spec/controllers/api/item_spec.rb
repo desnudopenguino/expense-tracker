b95ada3008d4df58263d39d5938d3afd54a18644
Bucky Townsend <adam@desnudopenguino.com>
2018-07-07 22:06:49 -0700

    removed lines to set the json variable

--- spec/controllers/api/item_spec.rb
+++ spec/controllers/api/item_spec.rb
@@ -3,14 +3,12 @@ RSpec.describe Api::ItemsController do
 	it "GET data for all items" do
 		rec = create(:receipt_with_items)
 		get :index, params: { receipt_id: rec.id }
-		json = JSON.parse(response.body)
 		expect(response).to be_success
 		expect(json.length).to eq(3)
 	end
 	it "GET data for individual item" do
 		itm = create(:item)
 		get :show, params: { receipt_id: itm.receipt_id, id: itm.id }
-		json = JSON.parse(response.body)
 		expect(response).to be_success
 		expect(json['name']).to eq(itm.name)
 	end
@@ -18,7 +16,6 @@ RSpec.describe Api::ItemsController do
 		rec = create(:receipt)
 		attrs = attributes_for(:item)
 		post :create, params: { receipt_id: rec.id, item: attrs }
-		json = JSON.parse(response.body)
 		expect(response).to be_success
 		expect(json['name']).to eq(attrs[:name])
 	end
@@ -26,7 +23,6 @@ RSpec.describe Api::ItemsController do
 		itm = create(:item)
 		new_name = "item2"
 		patch :update, params: { receipt_id: itm.receipt_id, id: itm.id, item: { name: new_name } }
-		json = JSON.parse(response.body)
 		expect(response).to be_success
 		expect(json['id']).to eq(itm.id)
 		expect(json['name']).to eq(new_name)
